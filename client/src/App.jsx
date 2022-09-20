a35309ea6430454ad900c7a8c27803cededee843
dormirMais <74685291+dormirMais@users.noreply.github.com>
2022-09-21 00:02:50 +0900

    [Add] ItemCard 찜하기 아이콘 완성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import CartPage from "./pages/CartPage";
+import ItemCard from "./components/Commons/ItemCard";
+
 
 
 
@@ -25,6 +27,7 @@ function App() {
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="/cart" element={<CartPage />} />
           </Routes>
+          <ItemCard />
         </MainContainer>
       </BrowserRouter>
 
--- client/src/components/Commons/ItemCard.jsx
+++ client/src/components/Commons/ItemCard.jsx
@@ -1,9 +1,45 @@
 /* eslint-disable */
 
-import React from "react";
+import React, { useState } from "react";
 // eslint-disable-next-line
 import styled from "styled-components";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+
+
+
+// eslint-disable-next-line
+function ItemCard(props) {
+
+    const [isClicked, setIsClicked] = useState(false)
+
+    const handleClicked = (e) =>{
+       
+        setIsClicked(curr => !curr);
+        e.preventDefault();
+        
+    }
+  return (
+    <Container>
+      <a href="https://naver.com">
+        <div className="ItemCard-Image">
+            {isClicked ?  <AiFillHeart onClick={handleClicked} className="ItemCard-Heart"/> :  <AiOutlineHeart onClick={handleClicked} className="ItemCard-Heart" />}
+        </div> 
+        <div className="ItemCard-Explain">
+            
+            {/* <p className='ItemCard-Explain__brand'>{props.brand}</p>
+                    <p className='ItemCard-Explain__title'>{props.title}</p>
+                    <p className='ItemCard-Explain__price'>{props.price}</p> */}
+            <p className="ItemCard-Explain__brand">Brand</p>
+            <p className="ItemCard-Explain__title">Title</p>
+            <p className="ItemCard-Explain__price">
+                Price <br /><p>즉시구매가</p>
+            </p>
+        
+        </div>
+      </a>
+    </Container>
+  );
+}
 
 let Container = styled.div`
   width: 290px;
@@ -27,14 +63,20 @@ let Container = styled.div`
     border-radius: 10px;
     width: 290px;
     height: 290px;
+    background-image: url(https://codingapple1.github.io/shop/shoes1.jpg);
+    background-size: 100% 100%;
+    background-position: center;   
   }
 
   .ItemCard-Heart {
     position: relative;
-    top: -50px;
-    left: 250px;
+    top: 240px;
+    left: 230px;
     font-size: 30px;
     margin-bottom: 0px;
+    z-index: 100;
+    color: pink;
+    padding: 3px;
   }
 
   .ItemCard-Explain {
@@ -62,33 +104,5 @@ let Container = styled.div`
     }
   }
 `;
-
-// eslint-disable-next-line
-function ItemCard(props) {
-  return (
-    <Container>
-      <a href="https://naver.com">
-        <img
-          className="ItemCard-Image"
-          src={`https://codingapple1.github.io/shop/shoes1.jpg`}
-          width="100%"
-        ></img>
-      </a>
-      <div className="ItemCard-Explain">
-        <a href="https://naver.com">
-          {/* <p className='ItemCard-Explain__brand'>{props.brand}</p>
-                    <p className='ItemCard-Explain__title'>{props.title}</p>
-                    <p className='ItemCard-Explain__price'>{props.price}</p> */}
-          <p className="ItemCard-Explain__brand">Brand</p>
-          <p className="ItemCard-Explain__title">Title</p>
-          <p className="ItemCard-Explain__price">
-            Price <br />
-            <p>즉시구매가</p>
-          </p>
-        </a>
-      </div>
-    </Container>
-  );
-}
 
 export default ItemCard;
