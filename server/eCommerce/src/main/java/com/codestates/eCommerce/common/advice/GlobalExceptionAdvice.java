80701a2b47e3a0bbc4662baf6af6cd10d92a3b68
subo-9439 <kws3363@gmail.com>
2022-10-03 01:17:38 +0900

    [Add] 에러 메세지 관리기능 추가

--- server/eCommerce/src/main/java/com/codestates/eCommerce/common/advice/GlobalExceptionAdvice.java
+++ server/eCommerce/src/main/java/com/codestates/eCommerce/common/advice/GlobalExceptionAdvice.java
@@ -1,8 +1,10 @@
 package com.codestates.eCommerce.common.advice;
 
+import com.codestates.eCommerce.common.config.matterMost.NotificationManager;
 import com.codestates.eCommerce.common.exception.BusinessLogicException;
 import com.codestates.eCommerce.common.exception.product.ProductBusinessExcepion;
 import com.codestates.eCommerce.order.domain.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +16,42 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
+import java.util.Enumeration;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 @Slf4j
 public class GlobalExceptionAdvice {
+    private final NotificationManager notificationManager;
+
+    /* MatterMost 외부 메세지 */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity exceptionTest(Exception e, HttpServletRequest req) {
+        e.printStackTrace();
+        notificationManager.sendNotification(e, req.getRequestURI(), getParams(req));
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private String getParams(HttpServletRequest req) {
+        StringBuilder params = new StringBuilder();
+        Enumeration<String> keys = req.getParameterNames();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            params.append("- ").append(key).append(" : ").append(req.getParameter(key)).append("/n");
+        }
+
+        return params.toString();
+    }
+
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
-
+        notificationManager.sendNotification(e);
         return response;
     }
 
@@ -86,15 +114,15 @@ public class GlobalExceptionAdvice {
         return response;
     }
 
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException(Exception e) {
-        log.error("# handle Exception", e);
-        // TODO 애플리케이션의 에러는 에러 로그를 로그에 기록하고, 관리자에게 이메일이나 카카오 톡,
-        //  슬랙 등으로 알려주는 로직이 있는게 좋습니다.
-
-        final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
-
-        return response;
-    }
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public ErrorResponse handleException(Exception e) {
+//        log.error("# handle Exception", e);
+//        // TODO 애플리케이션의 에러는 에러 로그를 로그에 기록하고, 관리자에게 이메일이나 카카오 톡,
+//        //  슬랙 등으로 알려주는 로직이 있는게 좋습니다.
+//
+//        final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
+//
+//        return response;
+//    }
 }
