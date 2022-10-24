ac848c309778cb4f098c1af3a4c03567ba068da6
clover <ThreeLeafClover9@gmail.com>
2022-10-24 09:41:01 +0900

    [Fix] OAuth CORS 해결

--- server/eCommerce/src/main/java/com/codestates/eCommerce/security/config/SecurityConfig.java
+++ server/eCommerce/src/main/java/com/codestates/eCommerce/security/config/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.codestates.eCommerce.security.config;
 
 import com.codestates.eCommerce.common.config.matterMost.NotificationManager;
-import com.codestates.eCommerce.member.repository.MemberRepository;
 import com.codestates.eCommerce.member.mapper.MemberMapper;
+import com.codestates.eCommerce.member.repository.MemberRepository;
 import com.codestates.eCommerce.security.jwt.JwtAuthenticationFilter;
 import com.codestates.eCommerce.security.jwt.JwtAuthorizationFilter;
-import com.codestates.eCommerce.security.jwt.JwtFilter;
-import com.codestates.eCommerce.security.oauth.OAuth2FailureHandler;
 import com.codestates.eCommerce.security.oauth.OAuth2SuccessHandler;
-import com.codestates.eCommerce.security.oauth.PrincipalOauth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,57 +16,59 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.CorsFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity(debug = true) // 스프링 시큐리티 필터(SecurityConfig)가 스프링 필터 체인에 등록
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // @Secured 활성화, @PreAuthorize & @PostAuthorize 활성화
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final CorsFilter corsFilter;
-    private final MemberRepository repository;
-    private final PrincipalOauth2UserService principalOauth2UserService;
-    private final OAuth2SuccessHandler successHandler;
-    private final OAuth2FailureHandler failureHandler;
-    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-    private final CustomAccessDeniedHandler accessDeniedHandler;
-    private final CustomAuthenticationEntryPoint entryPoint;
-    private final JwtFilter jwtFilter;
     private final MemberMapper mapper;
+    private final MemberRepository repository;
     private final NotificationManager notificationManager;
+    private final CustomAuthenticationEntryPoint entryPoint;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http
+                .headers().frameOptions().sameOrigin()
                 .and()
-                .addFilter(corsFilter) // @CrossOrigin(인증X), 인증(O) 필터에 등록
+                .csrf().disable() // @CrossOrigin(인증X), 인증(O) 필터에 등록
+                .cors(withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .formLogin().disable()
+                .httpBasic().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(entryPoint)
+//                .accessDeniedHandler()
                 .and()
-
-                .httpBasic().disable()
-                .apply(new CustomDsl())
+                .apply(new CustomFilterConfigurer())
                 .and()
-                .authorizeRequests()
-                .antMatchers(
-                        "/api/v1/carts/**",
-                        "/api/v1/bookmarks/**",
-                        "/api/v1/members/{member-id}/**"
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers(
+                                "/api/v1/carts/**",
+                                "/api/v1/bookmarks/**",
+                                "/api/v1/members/{member-id}/**"
+                        ).authenticated()
+                        .anyRequest().permitAll()
                 )
-                .authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(principalOauth2UserService);
+                .oauth2Login(oauth2 -> oauth2
+//                        .successHandler(new OAuth2MemberSuccessHandler(jwtTokenizer, authorityUtils, memberService))
+                        .successHandler(new OAuth2SuccessHandler(mapper))
 
-        http.csrf().disable();
+                );
+
         return http.build();
     }
 
-    public class CustomDsl extends AbstractHttpConfigurer<CustomDsl, HttpSecurity> {
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
         @Override
         public void configure(HttpSecurity builder) throws Exception {
+//            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+//
+//            builder.addFilterAfter(jwtVerificationFilter, OAuth2LoginAuthenticationFilter.class);
             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
             builder
                     .addFilter(new JwtAuthenticationFilter(authenticationManager, mapper))
